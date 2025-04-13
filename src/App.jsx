8f1fd68f3fe2767b3dabfc10daa0ca5e2ebb8258
zhaoding <18502962139@163.com>
2025-04-13 23:44:02 +0800

    TODO 静态页面

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import Header from './components/Header';
+import List from './components/List';
+import Footer from './components/Footer';
+
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        App...
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header/>
+          <List />
+          <Footer />
+        </div>
       </div>
     )
   }
