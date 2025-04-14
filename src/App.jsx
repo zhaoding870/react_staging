03ca31eac176cd5237e6775b1b71c61eda9e0360
zhaoding <18502962139@163.com>
2025-04-14 23:14:16 +0800

    加载动态数据

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,19 @@ import Footer from './components/Footer';
 import "./App.css";
 
 export default class App extends Component {
+
+  state = {todos:[
+    {id: '001', name: '吃饭', done: true},
+    {id: '002', name: '睡觉', done: true},
+    {id: '003', name: '打代码', done: false},
+  ]}
   render() {
+    const {todos} = this.state;
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header/>
-          <List />
+          <List todos={todos}/>
           <Footer />
         </div>
       </div>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -6,11 +6,14 @@ import './index.css'
 
 export default class List extends Component {
   render() {
+    const {todos} = this.props;
     return (
       <ul className="todo-main">
-        <Item />
-        <Item />
-        <Item />
+        {
+          todos.map( todo => {
+            return <Item {...todo} key={todo.id}/>
+          } )
+        }
       </ul>
     )
   }
