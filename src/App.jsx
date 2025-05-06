c45bf8a05bd1a58a97c384dbc3dd251d22d201d3
zhaoding <18502962139@163.com>
2025-05-07 06:48:22 +0800

    初始化

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
-import Search from './components/Search'
-import List from './components/List'
-import './App.css';
-
 export default class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <Search />
-                <List />
+            <div>
+                App...
             </div>
         )
     }
