d5f7bd4f54aabda8d487b5a56eab89c92a0a1cea
zhaoding <18502962139@163.com>
2025-05-08 20:07:24 +0800

    NavLink 的使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {Link, Route} from 'react-router-dom';
+import {NavLink, Route} from 'react-router-dom';
 
 import About from './pages/About';
 import Home from './pages/Home';
@@ -21,8 +21,8 @@ export default class App extends Component {
                         <div className="list-group">
                             {/* <a className="list-group-item active" href="./about.html">About</a>
                             <a className="list-group-item" href="./home.html">Home</a> */}
-                            <Link className="list-group-item" to="/about">About</Link>
-                            <Link className="list-group-item" to="/home">Home</Link>
+                            <NavLink activeClassName="active_cur" className="list-group-item" to="/about">About</NavLink>
+                            <NavLink activeClassName="active_cur" className="list-group-item" to="/home">Home</NavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
