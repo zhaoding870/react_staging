5e26508bb324d5cbb2d1f3ade203e1f1313910b4
zhaoding <18502962139@163.com>
2025-05-08 20:09:03 +0800

    NavLink

---
+++ 09_src_NavLink的使用/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import {NavLink, Route} from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Header from './components/Header';
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <Header />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            {/* <a className="list-group-item active" href="./about.html">About</a>
+                            <a className="list-group-item" href="./home.html">Home</a> */}
+                            <NavLink activeClassName="active_cur" className="list-group-item" to="/about">About</NavLink>
+                            <NavLink activeClassName="active_cur" className="list-group-item" to="/home">Home</NavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                <Route path="/about" component={About}/>
+                                <Route path="/home" component={Home}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
